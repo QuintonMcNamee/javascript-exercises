6c75fe73852903d2e983c3becea3c6ec1f08f13a
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2020-05-12 18:15:03 -0400

    exercise 6 complete

--- exercises/MaxChar/index.js
+++ exercises/MaxChar/index.js
@@ -10,7 +10,32 @@
  */
 
 function maxChar(str) {
-
+    let longest = 0;
+    let longestCurrent = 0;
+    let char = '';
+    let charLongest = '';
+    console.log(str);
+    
+    while (str.length > 0) {
+        char = str.charAt(0);
+        // console.log(char);
+        for (let i = 0; i < str.length; i++) {
+            if (char === str[i]) {
+                longestCurrent += 1;
+                if (longestCurrent > longest) {
+                    longest = longestCurrent;
+                    charLongest = char;
+                };
+            };
+        };
+        longestCurrent = 0;
+        str = str.substr(1)
+        console.log(str);
+    };
+    console.log(charLongest);
+    return charLongest;
 }
+
+maxChar("apple 1231111");
 
 module.exports = maxChar;
